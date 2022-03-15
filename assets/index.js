69f0ffefd29475015b464fabb987529114783abd
Juan M Hernandez <juanhm93@gmail.com>
2022-03-14 22:58:22 -0500

    add course

---
+++ assets/index.js
@@ -0,0 +1,12 @@
+import MediaPlayer from './MediaPlayer.js';
+import AutoPlay from './plugins/AutoPlay.js';
+
+const video = document.querySelector("video");
+const playButton = document.querySelector("#playButton");
+const muteButton = document.querySelector("#muteButton");
+
+
+const player = new MediaPlayer({el: video, plugins: [new AutoPlay()]});
+
+playButton.onclick = () => player.tooglePlay();
+muteButton.onclick = () => player.toogleMute();
